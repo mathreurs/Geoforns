26ecfde349fb4cd228a8cad145a6a91bdc5ed8aa
mathreur <mathimor127@hotmail.com>
2023-12-07 08:44:03 -0300

    1.2 quase pronto

--- GeoIinks/Assets/Scripts/PosGuerraFriaManager.cs
+++ GeoIinks/Assets/Scripts/PosGuerraFriaManager.cs
@@ -1,141 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PosGuerraFriaManager : MonoBehaviour
 {
-    public GameObject painel1,painel2,painel3, painel4,painel5,painel6,painel7,painel8,painel9,painel10, painel11, painel12, painel13, painel14, painel15, painel16, painel17, painel18, painel19, painel20, painel21, painel22, painel23, painel24, painel25, painel26, painel27, painel28, painel29, painel30,painel31,painel32;
-    public void passSlide()
-    {
+    public GameObject slide1,slide2,slide3,slide4,slide5,atualSlide,proxSlide, slideFinal;
 
+    public float certo, errado;
+
+    public void Update()
+    {
+        if(slide1.activeInHierarchy)
+        {
+            atualSlide = slide1;
+            proxSlide = slide2;
+        }
+
+        if(slide2.activeInHierarchy)
+        {
+            atualSlide = slide2;
+            proxSlide = slide3;
+        }
+
+        if (slide3.activeInHierarchy)
+        {
+            atualSlide = slide3;
+            proxSlide = slide4;
+        }
+
+        if (slide4.activeInHierarchy) 
+        {
+            atualSlide = slide4;
+            proxSlide = slide5;
+        }
+
+        if (slide5.activeInHierarchy)
+        {
+            atualSlide = slide5;
+            proxSlide = slideFinal;
+        }
     }
-    public void passSlide1()
-    {
 
+    public void correto()
+    {
+        atualSlide.SetActive(false);
+        proxSlide.SetActive(true);
+        certo++;
     }
-    public void passSlide2()
-    {
 
-    }
-    public void passSlide3()
+    public void erro()
     {
-
-    }
-    public void passSlide4()
-    {
-
-    }
-    public void passSlide5()
-    {
-
-    }
-    public void passSlide6()
-    {
-
-    }
-    public void passSlide7()
-    {
-
-    }
-    public void passSlide8()
-    {
-
-    }
-    public void passSlide9()
-    {
-
-    }
-    public void passSlide10()
-    {
-
-    }
-    public void passSlide11()
-    {
-
-    }
-    public void passSlide12()
-    {
-
-    }
-    public void passSlide13()
-    {
-
-    }
-    public void passSlide14()
-    {
-
-    }
-    public void passSlide15()
-    {
-
-    }
-    public void passSlide16()
-    {
-
-    }
-    public void passSlide17()
-    {
-
-    }
-    public void passSlide18()
-    {
-
-    }
-    public void passSlide19()
-    {
-
-    }
-    public void passSlide20()
-    {
-
-    }
-    public void passSlide21()
-    {
-
-    }
-    public void passSlide22()
-    {
-
-    }
-    public void passSlide23()
-    {
-
-    }
-    public void passSlide24()
-    {
-
-    }
-    public void passSlide25()
-    {
-
-    }
-    public void passSlide26()
-    {
-
-    }
-    public void passSlide27()
-    {
-
-    }
-    public void passSlide28()
-    {
-
-    }
-    public void passSlide29()
-    {
-
-    }
-    public void passSlide30()
-    {
-
-    }
-    public void passSlide31()
-    {
-
-    }
-    public void passSlide32()
-    {
-
+        atualSlide.SetActive(false);
+        proxSlide.SetActive(true);  
+        errado++;
     }
 
 }
